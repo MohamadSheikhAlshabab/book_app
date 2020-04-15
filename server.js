a2137f91cd11607e4f5495e194be154785a54713
Your Name In Single Quotes <your email here in single quotes>
2020-04-15 06:19:03 +0300

    edit on lab 12

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT || 4000;
 const cors = require('cors');
-const pg = require('pg');
+const pg = require('pg'); 
 const superagent = require('superagent');
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error',(err)=>console.log(err));
@@ -18,31 +18,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-
+  console.log('0000000000000');
   res.render('./pages/index');
 });
-// app.get('/',(req,res)=>{
-//     res.render('pages/index');
-// });
+
 app.get('/',getBooks);
 app.get('/books/:id',getOneBook);
 
 app.get('/searches/new', (req, res) => {
-  res.render('./pages/searches/new');
+  console.log('111111111111111111');
+  res.render('./pages/searches/show');
   
 });
-app.get('/error', (req, res) => {
-  throw new Error('./pages/error');
-});
-app.get('/show', (req, res) => {
-  res.render('pages/searches/show');
+app.get('/error', errorHandler);
+
+app.get('/addShow', (req, res) => {
+  console.log('3333333333333333333333');
+  res.render('./pages/searches/show');
 });
 app.get('/hello', (req, res) => {
+  console.log('44444444444444444444444444');
   res.render('./pages/index');
 });
 
 
-app.get('/searches', (req, res) => {
+app.get('/addNew', (req, res) => {
+  console.log('6666666666666666666666');
   res.render('./pages/searches/new');
 });
 app.post('/books',addBook);
@@ -53,17 +54,19 @@ app.post('/searches', (req, res) => {
     console.log(req.body.radioInput);
   let SearchForm = req.body.SearchForm;
   let radioInput = req.body.radioInput;
-  const url = `https://www.googleapis.com/books/v1/volumes?SearchForm=${req.body.SearchForm}&radioInput${req.body.radioInput}&key=${BOOKS_API_KEY}`;
+  // const url = `https://www.googleapis.com/books/v1/volumes?SearchForm=${req.body.SearchForm}&radioInput${req.body.radioInput}&key=${BOOKS_API_KEY}`;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${SearchForm}+in${radioInput}`;
   superagent.get(url).then((apiResponse) => {
-    console.log(apiResponse.body.items[0]);
-    console.log(path);
-    let book = apiResponse.body.items;
-    let books = book.map(item => {
-      return new Books(item);
+    //x
+    let bookData = apiResponse.body.items;
+    let bookks = bookData.map(itemes => {
+      return new Books(itemes);
     })
-    response.render('./pages/searches/show', { bookChoice: books });
-  }).catch((err) =>{ errorHandler(err, req, res)
-  console.log('here error handler from catch')});
+    console.log(bookks); 
+    console.log('hello')
+    res.render('./pages/searches/show', { abookTable: bookks });
+  })
+  .catch((err) =>{ errorHandler(err, req, res)});
 })
 
 
@@ -79,11 +82,11 @@ client.query(SQL,values).then(results=>{
 });
 };
 
-function Books(abook) {
-  this.title = (abook.volumeInfo.title)?abook.volumeInfo.title:"can't find the Title "; 
-  this.author = (abook.volumeInfo.author)?abook.volumeInfo.author:"can't find the Author ";
-  this.description = (abook.volumeInfo.description)?abook.volumeInfo.description:" There is no a description";
-  this.image =(abook.volumeInfo.imageLinks)?abook.volumeInfo.imageLinks.smallThumbnail:'https://images.vexels.com/media/users/3/157545/isolated/preview/057098b4a63e172134e0f04bbbcd6e8b-school-book-icon-by-vexels.png';
+function Books(data) {
+  this.title = data.volumeInfo.title? data.volumeInfo.title: "No Title Available";
+    this.imgUrl = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) ? data.volumeInfo.imageLinks.thumbnail:"https://i.imgur.com/J5LVHEL.jpg";
+    this.authors = data.volumeInfo.authors? data.volumeInfo.authors: "No Authors";
+    this.desc = data.volumeInfo.description? data.volumeInfo.description:"No description available";
 }
 
 function getBooks(req,res){
@@ -99,7 +102,7 @@ function getOneBook(req,res){
   const SQL='SELECT * FROM books WHERE id=$1;'
   const vals=[req.params.id];
   client.query(SQL,vals).then((results)=>{
-    res.render('/pages/books/detail',{oneBook:results.row[0]});
+    res.render('../pages/books/detail',{oneBook:results.row[0]});
   }).catch(err=>{
     errorHandler(err,req,res);
   })
@@ -113,8 +116,8 @@ app.use('*',notFoundHandler);
 app.listen(PORT, () => { console.log(`Server is Running on PORT ${PORT}`) });
 
 function errorHandler(err, req, res) {
-  res.status(500).render('./pages/error');
   console.log('here error handler');
+  res.status(500).render('./pages/error');
 }
 function notFoundHandler(req, res) {
   res.status(404).send('Page Not Found !!!!!!!!');
